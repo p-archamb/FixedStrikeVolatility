549ac555fda438005513384404decbed657610d9
pierrearchambeau <psarchambeau@dons.usfca.edu>
2024-10-08 17:55:24 -0700

    Initial commit

---
+++ src/main/java/com/example/FixedStrikeVolatility/FixedStrikeVolatilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.FixedStrikeVolatility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedStrikeVolatilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedStrikeVolatilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FixedStrikeVolatility/FixedStrikeVolatilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FixedStrikeVolatility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixedStrikeVolatilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
